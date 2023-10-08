9a8ac45bfbcc6b46f29411bf46407d27094488ae
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-08 22:37:57 +0530

    typeof

---
+++ assignment/assignment_2.js
@@ -0,0 +1,8 @@
+let a = 2
+let b = 3.4
+//typeof to know what they are 
+//In js there is  no any concept of int or float it look all as number 
+console.log(typeof a)
+console.log(a * b)
+console.log(a + b)
+console.log(45 + b)
