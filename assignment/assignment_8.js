0ba4fa8c9495fac969ea558a9b9f79a682916392
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-08 23:15:44 +0530

    boolean

---
+++ assignment/assignment_8.js
@@ -0,0 +1,11 @@
+let a = true
+let b = false
+console.log(typeof a)
+console.log(a)
+//typeof to know is that boolean.
+console.log(typeof b)
+console.log(b)
+//using  and operator for boolean 
+console.log(a && b)
+//using or operator for boolean
+console.log(a || b)
