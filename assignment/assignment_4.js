16f77ed4e7a6831b4b5e74faa4aca004293c8e4d
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-08 22:56:27 +0530

    string

--- assignment/assignment_4.js
+++ assignment/assignment_4.js
@@ -2,5 +2,7 @@
 let a = 'Hello World "How are you"'
 let b = "Good bye"
 console.log(a)
+console.log(typeof a)
 console.log(b)
+console.log(typeof b)
 //we can use single or double quote for string in js.
