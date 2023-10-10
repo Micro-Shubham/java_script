82591d0af1eef9e6f44862846805ccdbe96e828d
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-10-10 22:25:20 +0530

    funtion call to function

---
+++ assignment/assignment_20.js
@@ -0,0 +1,8 @@
+function printvariable(variable) {
+    console.log(variable)
+}
+function fun(x) {
+    var x = "Hello world"
+    printvariable(x)//call the printvarible with string hello world
+}
+fun() //call the funtion fun.
