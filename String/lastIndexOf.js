0eda91ad4c7c5f53a50c4f0a68338ce77d69b70f
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-09 10:52:00 +0530

    lastIndexOf

---
+++ String/lastIndexOf.js
@@ -0,0 +1,7 @@
+function findlastindex(str, target) {
+    console.log("Original string :", str);
+    console.log("index :", str.lastIndexOf(target));
+
+}
+findlastindex("Hello World World", "World");
+//give you index counting from starting but index of last W.
