3544bbbd8edcc091b729dbaba42b97b50c499ad0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-09 10:45:15 +0530

    indexOF(target)

---
+++ String/findIndexOf.js
@@ -0,0 +1,6 @@
+function findIndexOf(str, target) {
+    //str.indexOf(target) give you the index number from starting 
+    console.log("index of string :", str.indexOf(target));
+    console.log("Original string :", str);
+}
+findIndexOf("Hello World", "World");
---
+++ String/getlength.js
@@ -0,0 +1,5 @@
+function getlength(str) {
+    console.log("original string :", str);
+    console.log("Length of string :", str.length);
+}
+getlength("hello world")
