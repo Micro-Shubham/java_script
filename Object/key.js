5d6bed033e1cabe0e440365d4097d2ae0343b5e0
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-12 17:15:13 +0530

    object

---
+++ Object/key.js
@@ -0,0 +1,11 @@
+//declaration of object
+let person = {
+    Name: "kyle",
+    age: 24,
+    food: "panner"
+
+}
+//log all the key and its value 
+console.log(person);
+//accessing specific key
+console.log(person.Name +" "+person.age);
