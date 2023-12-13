91fa7ce6de24c2a0cbada7594a5f841072bbd749
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-13 22:19:14 +0530

    foreach

--- Array/forEach.js
+++ Array/forEach.js
@@ -1,4 +1,12 @@
-let arr = [1, 2, 3, 4, 5, 6, 7];
-arr.forEach((element, index) => {
-    console.log(element + " " + index);
-});
+// let arr = [1, 2, 3, 4, 5, 6, 7];
+// arr.forEach((element, index) => {
+//     console.log(element + " " + index);
+// });
+function array(arr) {
+    console.log("original array :", arr);
+    arr.forEach(function (index, element) {
+        console.log(index + " " + element)
+    })
+}
+let arr = [1, 2, 3, 4, 5, 6];
+array(arr);
