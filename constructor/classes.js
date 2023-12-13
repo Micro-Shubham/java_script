1241aa44b9d7dfe5b87ac4a9ef9be8ba03fffe5b
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-14 01:23:39 +0530

    classes

---
+++ constructor/classes.js
@@ -0,0 +1,9 @@
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+        this.human = true;
+    }
+}
+const user = new User("harkirat singh ", 26);
+console.log(user);
