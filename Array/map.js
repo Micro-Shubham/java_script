982213598cfd4ca95de2f070c226565699d860ad
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-13 22:31:10 +0530

    map

--- Array/map.js
+++ Array/map.js
@@ -1,9 +1,15 @@
-function array(arr) {
-    console.log("original array ", arr);
-    let newarray = arr.map(function (element) {
-        return element * 2;
-    })
-    console.log("after map ", newarray);
-}
-let arrr = [1, 2, 4, 5, 6, 7];
-array(arrr);
+// function array(arr) {
+//     console.log("original array ", arr);
+//     let newarray = arr.map(function (element) {
+//         return element * 2;
+//     })
+//     console.log("after map ", newarray);
+// }
+// let arrr = [1, 2, 4, 5, 6, 7];
+// array(arrr);
+let arr = [1, 2, 3, 4, 5, 6];
+let newarray = arr.map(element => {
+    return element * 2
+})
+
+console.log(newarray);
