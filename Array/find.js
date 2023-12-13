fe250a4378e4c1039350b69f7b81d04435f19414
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-13 22:46:06 +0530

    find

--- Array/find.js
+++ Array/find.js
@@ -1,5 +1,14 @@
 let array = [1, 2, 3, 4, 5, 7];
-let n = array.find(number => {
-    return number > 2
-})
-console.log(n);
+// let n = array.find(number => {
+//     return number > 2
+// })
+// console.log(n);
+
+function newarray(arr) {
+    let n = arr.find(function (element) {
+        return element > 2;
+    })
+    console.log(n);
+}
+
+newarray(array);
