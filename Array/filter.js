1580b507bb32b1127be962132ed6691109ab0155
Shubham Yadav <shubhamtikkha09@gmail.com>
2023-12-13 22:38:25 +0530

    fileter

--- Array/filter.js
+++ Array/filter.js
@@ -1,8 +1,13 @@
-function filterexample(arr) {
-    let afterfilter = arr.filter(function (element) {
-        return element <= 2
-    })
-    console.log(afterfilter);
-}
-let array = [1, 2, 3, 4, 5, 6, 7];
-filterexample(array);
+// function filterexample(arr) {
+//     let afterfilter = arr.filter(function (element) {
+//         return element <= 2
+//     })
+//     console.log(afterfilter);
+// }
+// let array = [1, 2, 3, 4, 5, 6, 7];
+// filterexample(array);
+let array = [1, 2, 3, 4, 5, 6, 6];
+let newarray = array.filter(element => {
+    return element > 2;
+})
+console.log(newarray);
